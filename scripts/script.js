afc2fec2a8493b8f937d622af01ef7b02129037e
Ivan Stepalin <uncabron121@yandex.ru>
2020-11-25 22:24:18 +0300

    Работа 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,30 @@
 let popup = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.form__close-icon');
+let closeButton = document.querySelector('.popup-box__close-icon');
+let submitButton = document.querySelector('.popup-box__submit-button');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
-let nameBox = document.querySelector('.form__input-box');
+let nameBox = document.querySelector('.popup-box__input-box');
+let formElement = document.querySelector('.popup-box');
+let nameInput = formElement.querySelector('.popup-box__field_value_name');
+let jobInput = formElement.querySelector('.popup-box__field_value_job');
 
 function popupOn() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 function popupOff() {
     popup.classList.remove('popup_opened');
 }
-
-function popupOpen() {
-    nameBox.innerHTML = `<input type="text" value="${profileName.textContent}" class="form__field form__field_name">
-                                <input type="text" value="${profileJob.textContent}" class="form__field form__field_job">`;
-}
-buttonEdit.addEventListener('click', popupOpen);
-
-buttonEdit.addEventListener('click', popupOn);
-closeButton.addEventListener('click', popupOff);
-
-let formElement = document.querySelector('.form');
 function formSubmitHandler (event) {
     event.preventDefault(); 
-    let nameInput = formElement.querySelector('.form__field_name');
-    let jobInput = formElement.querySelector('.form__field_job');
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    
 }
+
 formElement.addEventListener('submit', formSubmitHandler); 
-formElement.addEventListener('submit', popupOff); 
+submitButton.addEventListener('click', popupOff);
+buttonEdit.addEventListener('click', popupOn);
+closeButton.addEventListener('click', popupOff);
 
